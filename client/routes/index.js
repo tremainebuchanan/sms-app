c6cb491223d98f2bc7553ef2ce888a278b4d252c
govjmuser <tremainekbuchanan@gmail.com>
2017-12-17 19:07:34 -0500

    Added user-service

--- client/routes/index.js
+++ client/routes/index.js
@@ -1,10 +1,7 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+'use strict'
+const express = require('express')
+const router = express.Router()
+router.get('/', (req, res) =>{
+  res.render('index')
+})
+module.exports = router
